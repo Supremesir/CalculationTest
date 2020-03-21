13a97e0c690844e4438bd346943946882e89b0b4
Haofan Fang <haofanfang@gmail.com>
2020-03-21 22:04:09 +0800

    update AndroidViewModel

--- app/src/main/java/com/supremesir/calculationtest/MyViewModel.java
+++ app/src/main/java/com/supremesir/calculationtest/MyViewModel.java
@@ -1,9 +1,12 @@
 package com.supremesir.calculationtest;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.SavedStateHandle;
 
 import java.util.logging.Handler;
@@ -16,10 +19,50 @@ import java.util.logging.Handler;
 public class MyViewModel extends AndroidViewModel {
 
     SavedStateHandle handle;
-    private static String KEY_HIGH_SCORE = "key_high_score";
+    private static final String KEY_HIGH_SCORE = "key_high_score";
+    private static final String KEY_LEFT_NUMBER = "key_left_number";
+    private static final String KEY_RIGHT_NUMBER = "key_right_number";
+    private static final String KEY_OPERATOR = "key_operator";
+    private static final String KEY_ANSWER = "key_answer";
+    private static final String SAVE_SHP_DATA_NAME = "save_shp_data_name";
+    private static final String KEY_CURRENT_SCORE = "key_current_score";
 
     public MyViewModel(@NonNull Application application, SavedStateHandle handle) {
         super(application);
-//        if (handle.contains())
+        if (!handle.contains(KEY_HIGH_SCORE)) {
+            SharedPreferences shp = getApplication().getSharedPreferences(SAVE_SHP_DATA_NAME, Context.MODE_PRIVATE);
+            handle.set(KEY_HIGH_SCORE, shp.getInt(KEY_HIGH_SCORE, 0));
+            handle.set(KEY_LEFT_NUMBER, 0);
+            handle.set(KEY_RIGHT_NUMBER, 0);
+            handle.set(KEY_OPERATOR, "+");
+            handle.set(KEY_ANSWER, 0);
+            handle.set(KEY_CURRENT_SCORE, 0);
+        }
+        this.handle = handle;
     }
+
+    MutableLiveData<Integer> getLeftNumber() {
+        return handle.getLiveData(KEY_LEFT_NUMBER);
+    }
+
+    MutableLiveData<Integer> getRightNumber() {
+        return handle.getLiveData(KEY_RIGHT_NUMBER);
+    }
+
+    MutableLiveData<String> getOperator() {
+        return handle.getLiveData(KEY_OPERATOR);
+    }
+
+    MutableLiveData<Integer> getAnswer() {
+        return handle.getLiveData(KEY_ANSWER);
+    }
+
+    MutableLiveData<Integer> getCurrentScore() {
+        return handle.getLiveData(KEY_CURRENT_SCORE);
+    }
+
+    void generator() {
+
+    }
+
 }
