9e378e6f74fe1c19aa18077ece9eb421a5088d31
Haofan Fang <haofanfang@gmail.com>
2020-03-22 16:14:50 +0800

    update TitleFragment

--- app/src/main/java/com/supremesir/calculationtest/TitleFragment.java
+++ app/src/main/java/com/supremesir/calculationtest/TitleFragment.java
@@ -2,11 +2,19 @@ package com.supremesir.calculationtest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.supremesir.calculationtest.databinding.FragmentTitleBinding;
 
 
 /**
@@ -14,15 +22,34 @@ import android.view.ViewGroup;
  */
 public class TitleFragment extends Fragment {
 
+    private MyViewModel myViewModel;
+    private FragmentTitleBinding binding;
+    private NavController controller;
+
     public TitleFragment() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        myViewModel = new ViewModelProvider(requireActivity(), new SavedStateViewModelFactory(requireActivity().getApplication(), requireActivity()))
+                .get(MyViewModel.class);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(requireActivity());
+
+        binding.titleBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controller = Navigation.findNavController(v);
+                controller.navigate(R.id.action_titleFragment_to_questionFragment);
+            }
+        });
+
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false);
+//        return inflater.inflate(R.layout.fragment_title, container, false);
     }
 }
