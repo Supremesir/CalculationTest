7acf3e52d53b28519399b8d63c105f849745d7f8
Haofan Fang <haofanfang@gmail.com>
2020-03-23 08:13:57 +0800

    update ViewModel and back button action

--- app/src/main/java/com/supremesir/calculationtest/MainActivity.java
+++ app/src/main/java/com/supremesir/calculationtest/MainActivity.java
@@ -1,14 +1,58 @@
 package com.supremesir.calculationtest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controller = Navigation.findNavController(this, R.id.fragment4);
+        NavigationUI.setupActionBarWithNavController(this, controller);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        if (controller.getCurrentDestination().getId() == R.id.questionFragment) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getString(R.string.quit_dialog_title));
+            builder.setPositiveButton(R.string.quit_dialog_positive_message, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    controller.navigateUp();
+                }
+            });
+            builder.setNegativeButton(R.string.quit_dialog_negative_message, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            builder.create().show();
+        } else {
+            controller.navigate(R.id.titleFragment);
+        }
+
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (controller.getCurrentDestination().getId() == R.id.titleFragment) {
+            super.onBackPressed();
+        } else {
+            onSupportNavigateUp();
+        }
     }
 }
